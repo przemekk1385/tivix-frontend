1f88eb68e4cef521ab302c59a6b3666a762d1aa2
Przemek Kaliś <przemek.kalis@gmail.com>
2021-02-20 23:06:35 +0100

    add login view and very basic movie view

---
+++ src/views/Login.vue
@@ -0,0 +1,98 @@
+<template>
+  <v-container fill-height fluid>
+    <v-row justify="center">
+      <v-col cols="6">
+        <v-form ref="loginForm" v-model="valid">
+          <v-text-field
+            v-model="username"
+            label="Username"
+            prepend-inner-icon="mdi-account"
+            :rules="[rules.required]"
+          >
+          </v-text-field>
+          <v-text-field
+            v-model="password"
+            label="Password"
+            prepend-inner-icon="mdi-lock"
+            :rules="[rules.required]"
+            type="password"
+          >
+          </v-text-field>
+
+          <v-btn color="primary" class="mt-6" @click="login()">
+            <v-icon left dark>
+              mdi-send
+            </v-icon>
+            Login
+          </v-btn>
+        </v-form>
+      </v-col>
+    </v-row>
+    <v-snackbar v-model="snackbar">
+      {{ text }}
+      <template v-slot:action="{ attrs }">
+        <v-btn color="pink" text v-bind="attrs" @click="snackbar = false">
+          Close
+        </v-btn>
+      </template>
+    </v-snackbar>
+  </v-container>
+</template>
+
+<script>
+import axios from "axios";
+
+export default {
+  name: "Login",
+  data: () => ({
+    username: "",
+    password: "",
+
+    rules: {
+      required: (value) => !!value || "Required",
+    },
+    valid: false,
+
+    snackbar: false,
+    text: "",
+  }),
+  watch: {
+    snackbar(val, oldVal) {
+      if (!val && oldVal) {
+        this.$refs.loginForm.resetValidation();
+      }
+    },
+  },
+  methods: {
+    async login() {
+      const host = "http://127.0.0.1:8000";
+      const tokenUri = "/auth/token/login";
+
+      if (this.valid || this.$refs.loginForm.validate()) {
+        try {
+          const authTokenPromise = await axios.post(
+            `${host}${tokenUri}`,
+            JSON.stringify({
+              username: this.username,
+              password: this.password,
+            }),
+            { headers: { "Content-Type": "application/json" } }
+          );
+          const { auth_token: authToken } = authTokenPromise?.data;
+
+          localStorage.setItem("authToken", authToken);
+          localStorage.setItem("username", this.username);
+
+          this.$router.push({ name: "movie" });
+        } catch (error) {
+          this.snackbar = true;
+          this.text = "Login failed.";
+        }
+      } else {
+        this.snackbar = true;
+        this.text = "Fill in required fields.";
+      }
+    },
+  },
+};
+</script>
---
+++ src/views/Movie.vue
@@ -0,0 +1,21 @@
+<template>
+  <v-container fluid> Movie View. Welcome {{ username }} </v-container>
+</template>
+
+<script>
+export default {
+  name: "Movie",
+  data: () => ({
+    authToken: "",
+    username: "",
+  }),
+  mounted() {
+    this.authToken = localStorage.getItem("authToken");
+    this.username = localStorage.getItem("username");
+
+    if (!this.authToken || !this.username) {
+      this.$router.push({ name: "login" });
+    }
+  },
+};
+</script>
