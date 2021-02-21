29d193e3ad5ab4a67d670e7cff7bc8063af01fce
Przemek Kaliś <przemek.kalis@gmail.com>
2021-02-21 21:46:14 +0100

    fix configuration

--- src/config.js
+++ src/config.js
@@ -1,9 +1,11 @@
-let apiHost = "";
+// let apiHost = "";
 
-if (process.env.NODE_ENV === "production") {
-  apiHost = "http://yourdomain.com/api/";
-} else {
-  apiHost = "http://127.0.0.1:8000/";
-}
+// if (process.env.NODE_ENV === "production") {
+//   apiHost = "http://yourdomain.com/api/";
+// } else {
+//   apiHost = "http://127.0.0.1:8000/";
+// }
 
-export default apiHost;
+// export default apiHost;
+
+export default "http://127.0.0.1:8000/";
