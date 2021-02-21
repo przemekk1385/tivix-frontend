82f7a007682e45e4dadf66e316447f84de454a27
Przemek Kaliś <przemek.kalis@gmail.com>
2021-02-21 21:25:24 +0100

    update

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,4 @@
 module.exports = {
   transpileDependencies: ["vuetify"],
-  publicPath: process.env.NODE_ENV === "production" ? "/foo" : "/",
+  publicPath: process.env.NODE_ENV === "production" ? "/" : "/",
 };
